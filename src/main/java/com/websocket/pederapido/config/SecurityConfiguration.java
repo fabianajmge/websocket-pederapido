e178a32831bb7a86bdbcd8fb82211b7f945ef569
Fabiana Santos <fabianajmge@yahoo.com.br>
2022-05-29 20:35:25 -0300

    t

--- src/main/java/com/websocket/pederapido/config/SecurityConfiguration.java
+++ src/main/java/com/websocket/pederapido/config/SecurityConfiguration.java
@@ -3,11 +3,11 @@ package com.websocket.pederapido.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-	
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -17,9 +17,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 				.authorizeRequests()
 				.antMatchers("/socket/**").permitAll()
 				.antMatchers("*").permitAll()
-	            .and().csrf().and()
-//	            .disable()
-//				.cors().configurationSource(request -> corsConfiguration)
+	            .and().csrf()
+	            .disable()
+	            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+	            .and().headers().frameOptions().sameOrigin()
+	            .and()
 				.addFilterBefore(new CorsFilter(), ChannelProcessingFilter.class);
 	}
 }
